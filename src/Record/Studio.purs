21f0f08f7eb603c21d2f79b9bcdf5fd1398a2f8f
Mark Eibes <mark.eibes@gmail.com>
2022-11-04 12:41:42 +0100

    Add `SameKeys`

--- src/Record/Studio.purs
+++ src/Record/Studio.purs
@@ -5,11 +5,13 @@ module Record.Studio
   , module Record.Studio.Merge
   , module Record.Studio.Keys
   , module Record.Studio.Shrink
+  , module Record.Studio.SameKeys
   ) where
 
 import Record.Studio.Sequence (SequenceRecord(..), sequenceRecord)
 import Record.Studio.Map (MapRecord(..), mapRecord)
 import Record.Studio.MapKind (MapRecordKind(..), mapRecordKind)
-import Record.Studio.Keys (class Keys, recordKeys)
+import Record.Studio.Keys (class Keys, keys, recordKeys)
 import Record.Studio.Shrink (shrink)
 import Record.Studio.Merge (mergeFlipped, (//))
+import Record.Studio.SameKeys (class SameKeys)
